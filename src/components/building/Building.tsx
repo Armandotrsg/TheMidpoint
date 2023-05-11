e2815ed4c49e114495023899bbdddd22c6da28bd
Armando Terrazas Gómez <A01640924@tec.mx>
2023-05-11 09:27:49 -0600

    Actualicé el título

--- src/components/building/Building.tsx
+++ src/components/building/Building.tsx
@@ -4,7 +4,7 @@ import maintenance from "../../assets/maintenance.svg";
 export default component$(() => {
     return (
         <section class="w-full h-full flex flex-col items-center justify-center text-center p-10">
-            <h1 class="text-4xl font-bold p-3 dark:text-white">Midpoint</h1>
+            <h1 class="text-4xl font-bold p-3 dark:text-white">The Midpoint</h1>
             <h2 class="text-2xl font-bold p-3 dark:text-white">Sitio en Construcción</h2>
             <div class="flex items-center justify-center mt-10">
                 <picture class="w-[60%] h-full flex-grow items-center justify-center self-center my-auto">
