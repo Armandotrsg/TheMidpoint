b0a3d4ef4bae26040a465fdb40d19e43d0773b51
Armando Terrazas Gómez <A01640924@tec.mx>
2023-05-30 17:53:19 -0600

    Trato de arreglar el botón flecha

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -17,7 +17,36 @@ export const Services = component$(() => {
                             Contamos con un amplio repertorio de servicios que
                             se adaptan a las necesidades de tu empresa.
                         </p>
-                        <ButtonArrow href="#contacto" text="Empieza ya" bgColor="blue" textColor="white" />
+                        {/* <a
+                            href={""}
+                            class={`relative inline-flex items-center justify-center mt-7 md:mt-4 px-12 py-3 overflow-hidden font-medium bg-blue-500 text-white transition duration-300 ease-out rounded shadow-lg group`}
+                        >
+                            <span
+                                class={`absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-500 group-hover:translate-x-0 ease-in-out`}
+                            >
+                                <svg
+                                    class="w-6 h-6"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span
+                                class={`absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease-in-out`}
+                            >
+                                Empieza ya
+                            </span>
+                            <span class="relative invisible">Empieza ya</span>
+                        </a> */}
+                        <ButtonArrow href={"#contacto"} text={"Empieza ya"} bgColor={"blue"} textColor={"white"} />
                     </div>
 
                     <div class="grid grid-cols-2 gap-4 sm:grid-cols-3">
